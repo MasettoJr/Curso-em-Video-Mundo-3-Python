a7170e8cb005724ee3e4c031435a2cb81770e842
Paulo Masetto <paulomasetto93@gmail.com>
2025-09-20 10:17:47 -0300

    Update ex083.py

--- ex083.py
+++ ex083.py
@@ -1,13 +1,1 @@
 expressao = input('Digite uma expressão: ')
-lista = []
-
-for letra in expressao:
-    lista.append(letra)
-
-if lista.count('(') == lista.count(')'):
-    print('Sua expressão é válida!')
-else:
-    print('Sua expressão está errada!')
-
-
-
