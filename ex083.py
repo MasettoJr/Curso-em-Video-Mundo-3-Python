13a7abaccee0f8ae500b6082a8ffb7a60edf0694
Paulo Masetto <paulomasetto93@gmail.com>
2025-09-21 12:09:45 -0300

    Update ex083.py

--- ex083.py
+++ ex083.py
@@ -1,1 +1,11 @@
 expressao = input('Digite uma expressão: ')
+parenteses = []
+for elemento in expressao:
+    if '(' not in parenteses:
+        parenteses.append('(')
+    elif elemento == ')':
+        parenteses.pop(0)
+if parenteses == []:
+    print('Sua expressão é válida!')
+else:
+    print('Sua expressão não é válida!')
