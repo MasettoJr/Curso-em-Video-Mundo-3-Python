1cf47d24ff28e4f29f20d26269f3203e6dccebec
Paulo Masetto <paulomasetto93@gmail.com>
2025-09-28 14:59:54 -0300

    Update ex087.py

--- ex087.py
+++ ex087.py
@@ -1,22 +1,39 @@
-from random import randint
-from time import sleep
-print('-' * 30)
-print('JOGUE NA MEGA SENA')
-print('-' * 30)
+a = []
+b = []
+c = []
+lista = [a, b, c]
 
-qtd_jogos = int(input('Quantos jogos você quer que eu sorteie? '))
-jogos = []
+for v in range(0, 9):
+    if v // 3 == 0:
+        a.append(int(input(f'Digite um valor para a [{v // 3}, {v % 3}]: ')))
+    elif v // 3 == 1:
+        b.append(int(input(f'Digite um valor para a [{v // 3}, {v % 3}]: ')))
+    else:
+        c.append(int(input(f'Digite um valor para a [{v // 3}, {v % 3}]: ')))
 
-for jogo in range(0, qtd_jogos):
-    jogos.append(list())
+print('-=-' * 10)
+for e in range(0, 9):
+    print(f'[ {lista[e // 3][e % 3]} ] ', end='')
+    if (e + 1) == 3 or (e + 1) == 6 or (e + 1) == 9:
+        print('')
+print('-=-' * 10)
 
-print(f'SORTEANDO {qtd_jogos} JOGOS')
-for i, jogo in enumerate(jogos):
-    while len(jogo) < 6:
-        num = randint(1, 60)
-        if num not in jogo:
-            jogo.append(num)
+soma = 0
+for elemento in lista:
+    for elemento2 in elemento:
+        if elemento2 % 2 == 0:
+            soma += elemento2
+print(f'A soma dos valores pares é {soma}.')
 
-    jogo.sort()
-    print(f'Jogo {i + 1}: {jogo}', end='\n')
-    sleep(1)
+soma3col = 0
+for elemento in lista:
+    for i, elemento2 in enumerate(elemento):
+        if i == 2:
+            soma3col += elemento2
+print(f'A soma dos valores da terceira coluna é {soma3col}.')
+
+maior_valor = 0
+for i, elemento in enumerate(lista):
+    if i == 1:
+        maior_valor = max(elemento)
+print(f'O maior valor da segunda linha é {maior_valor}.')
